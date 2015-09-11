a59bd57fc16b18a4dff143f3b9943421e5d4e7db
Ian Buchanan <ibuchanan@atlassian.com>
2015-09-10 21:18:11 -0400

    refactored team find methods to use Bitbucket entrypoints

--- pybitbucket/team.py
+++ pybitbucket/team.py
@@ -1,6 +1,7 @@
-from uritemplate import expand
-
-from pybitbucket.bitbucket import BitbucketBase, Client
+"""
+Provides a class for manipulating Team resources on Bitbucket.
+"""
+from pybitbucket.bitbucket import Bitbucket, BitbucketBase, Client
 
 
 class TeamRole(object):
@@ -13,32 +14,28 @@ class TeamRole(object):
 class Team(BitbucketBase):
     id_attribute = 'username'
 
+    """
+    A convenience method for finding teams by the user's role.
+    The method is a generator Team objects.
+    """
     @staticmethod
     def find_teams_for_role(role=TeamRole.ADMIN, client=Client()):
         if role not in TeamRole.roles:
             raise NameError(
                 "role '%s' is not in [%s]" %
                 (role, '|'.join(str(x) for x in TeamRole.roles)))
-        template = 'https://{+bitbucket_url}/2.0/teams{?role}'
-        url = expand(
-            template, {
-                'bitbucket_url': client.get_bitbucket_url(),
-                'role': role})
-        for team in client.remote_relationship(url):
-            yield team
+        return Bitbucket(client=client).teamsForRole(role=role)
 
+    """
+    A convenience method for finding a specific team.
+    In contrast to the pure hypermedia driven method on the Bitbucket
+    class, this method returns a User object, instead of the
+    generator.
+    """
     @staticmethod
     def find_team_by_username(username, client=Client()):
-        template = 'https://{+bitbucket_url}/2.0/teams/{username}'
-        url = expand(
-            template, {
-                'bitbucket_url': client.get_bitbucket_url(),
-                'username': username})
-        response = client.session.get(url)
-        if 404 == response.status_code:
-            return
-        Client.expect_ok(response)
-        return Team(response.json(), client=client)
+        return next(Bitbucket(client=client).teamByUsername(
+            username=username))
 
     @staticmethod
     def is_type(data):

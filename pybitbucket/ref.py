57d9f8959eb5137431b45132e6a3810f0f7308e3
Ian Buchanan <ibuchanan@atlassian.com>
2016-03-04 13:05:37 -0500

    added refs, tags, and branches resources

---
+++ pybitbucket/ref.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Defines the Ref, Tag, and Branch resources
+and registers those type with the Client.
+
+Classes:
+- Ref: represents the abstraction of a Git reference to a commit
+- Tag: represents the tag resource that references a specific commit
+- Branch: represents the branch resource that references a set of commits
+"""
+from pybitbucket.bitbucket import Bitbucket, BitbucketBase, Client
+
+
+class Ref(BitbucketBase):
+    id_attribute = 'name'
+
+    @staticmethod
+    def is_type(data):
+        # Ref is an abstraction so does not match on data
+        return False
+
+    @staticmethod
+    def find_refs_in_repository(
+            owner,
+            repository_name,
+            client=Client()):
+        """
+        A convenience method for finding refs in a repository.
+        The method is a generator Ref subtypes of Tag and Branch.
+        """
+        return Bitbucket(client=client).repositoryRefs(
+            owner=owner,
+            repository_name=repository_name)
+
+
+class Tag(Ref):
+    resource_type = 'tags'
+
+    @staticmethod
+    def is_type(data):
+        return (Tag.has_v2_self_url(data))
+
+    @staticmethod
+    def find_tags_in_repository(
+            repository_name,
+            owner=None,
+            client=Client()):
+        """
+        A convenience method for finding tags in a repository.
+        The method is a generator Tag objects.
+        """
+        owner = owner or client.get_username()
+        return Bitbucket(client=client).repositoryTags(
+            owner=owner,
+            repository_name=repository_name)
+
+    @staticmethod
+    def find_tag_by_ref_name_in_repository(
+            ref_name,
+            repository_name,
+            owner=None,
+            client=Client()):
+        """
+        A convenience method for finding a specific tag.
+        In contrast to the pure hypermedia driven method on the Bitbucket
+        class, this method returns a Tag object, instead of the
+        generator.
+        """
+        owner = owner or client.get_username()
+        return next(Bitbucket(client=client).repositoryTagByName(
+            owner=owner,
+            repository_name=repository_name,
+            ref_name=ref_name))
+
+
+class Branch(Ref):
+    resource_type = 'branches'
+
+    @staticmethod
+    def is_type(data):
+        return (Branch.has_v2_self_url(data))
+
+    @staticmethod
+    def find_branches_in_repository(
+            repository_name,
+            owner=None,
+            client=Client()):
+        """
+        A convenience method for finding branches in a repository.
+        The method is a generator Branch objects.
+        """
+        owner = owner or client.get_username()
+        return Bitbucket(client=client).repositoryBranches(
+            owner=owner,
+            repository_name=repository_name)
+
+    @staticmethod
+    def find_branch_by_ref_name_in_repository(
+            ref_name,
+            repository_name,
+            owner=None,
+            client=Client()):
+        """
+        A convenience method for finding a specific branch.
+        In contrast to the pure hypermedia driven method on the Bitbucket
+        class, this method returns a Branch object, instead of the
+        generator.
+        """
+        owner = owner or client.get_username()
+        return next(Bitbucket(client=client).repositoryBranchByName(
+            owner=owner,
+            repository_name=repository_name,
+            ref_name=ref_name))
+
+
+Client.bitbucket_types.add(Ref)
+Client.bitbucket_types.add(Tag)
+Client.bitbucket_types.add(Branch)
---
+++ tests/test_ref.py
@@ -0,0 +1,393 @@
+# -*- coding: utf-8 -*-
+from test_bitbucketbase import BitbucketFixture
+import json
+
+import httpretty
+from uritemplate import expand
+from pybitbucket.ref import (Ref, Tag, Branch)
+from pybitbucket.bitbucket import Bitbucket
+from pybitbucket.commit import Commit
+from pybitbucket.repository import Repository
+
+
+class RefFixture(BitbucketFixture):
+    # GIVEN: a class under test
+    class_under_test = 'Ref'
+
+    # GIVEN: An example object created from example data
+    @classmethod
+    def example_object(cls):
+        return Ref(
+            json.loads(cls.resource_data()),
+            client=cls.test_client)
+
+    # GIVEN: Example data attributes for a refs
+    owner = 'atlassian'
+    repository_name = 'python-bitbucket'
+
+
+class TagFixture(RefFixture):
+    # GIVEN: a class under test
+    class_under_test = 'Tag'
+
+    # GIVEN: An example object created from example data
+    @classmethod
+    def example_object(cls):
+        return Tag(
+            json.loads(cls.resource_data()),
+            client=cls.test_client)
+
+    # GIVEN: Example data attributes for a tag
+    name = 'v0.1.1'
+
+
+class BranchFixture(RefFixture):
+    # GIVEN: a class under test
+    class_under_test = 'Branch'
+
+    # GIVEN: An example object created from example data
+    @classmethod
+    def example_object(cls):
+        return Branch(
+            json.loads(cls.resource_data()),
+            client=cls.test_client)
+
+    # GIVEN: Example data attributes for a branch
+    name = 'bug/35-url-name-collision'
+
+
+class TestGettingTheStringRepresentationOfATag(TagFixture):
+    @classmethod
+    def setup_class(cls):
+        cls.repository_str = str(cls.example_object())
+
+    def test_string_is_not_the_default_format(self):
+        assert not self.repository_str.startswith('<')
+        assert not self.repository_str.endswith('>')
+
+    def test_string_has_the_class_name_and_id_attribute(self):
+        assert self.repository_str.startswith('Tag name:')
+
+
+class TestGettingTheStringRepresentationOfABranch(BranchFixture):
+    @classmethod
+    def setup_class(cls):
+        cls.repository_str = str(cls.example_object())
+
+    def test_string_is_not_the_default_format(self):
+        assert not self.repository_str.startswith('<')
+        assert not self.repository_str.endswith('>')
+
+    def test_string_has_the_class_name_and_id_attribute(self):
+        assert self.repository_str.startswith('Branch name:')
+
+
+class TestCheckingTheExampleTagData(TagFixture):
+    @classmethod
+    def setup_class(cls):
+        cls.data = json.loads(cls.resource_data())
+
+    def test_passes_the_type_check(self):
+        assert Tag.is_type(self.data)
+
+
+class TestCheckingTheExampleBranchData(BranchFixture):
+    @classmethod
+    def setup_class(cls):
+        cls.data = json.loads(cls.resource_data())
+
+    def test_passes_the_type_check(self):
+        assert Branch.is_type(self.data)
+
+
+class TestAccessingTagAttributes(TagFixture):
+    @classmethod
+    def setup_class(cls):
+        cls.response = cls.example_object()
+
+    def test_name_is_expected(self):
+        assert self.name == self.response.name
+
+    def test_target_is_a_commit(self):
+        assert isinstance(self.response.target, Commit)
+
+
+class TestAccessingBranchAttributes(BranchFixture):
+    @classmethod
+    def setup_class(cls):
+        cls.response = cls.example_object()
+
+    def test_name_is_expected(self):
+        assert self.name == self.response.name
+
+    def test_target_is_a_commit(self):
+        assert isinstance(self.response.target, Commit)
+
+    def test_repository_is_a_repository(self):
+        assert isinstance(self.response.repository, Repository)
+
+
+class TestDeletingATag(TagFixture):
+    @httpretty.activate
+    def test_response_is_not_an_exception(self):
+        httpretty.register_uri(
+            httpretty.DELETE,
+            self.resource_url(),
+            status=204)
+        result = self.example_object().delete()
+        assert result is None
+
+
+class TestDeletingABranch(BranchFixture):
+    @httpretty.activate
+    def test_response_is_not_an_exception(self):
+        httpretty.register_uri(
+            httpretty.DELETE,
+            self.resource_url(),
+            status=204)
+        result = self.example_object().delete()
+        assert result is None
+
+
+class TestFindingTagByNameInRepository(TagFixture):
+    @classmethod
+    def setup_class(cls):
+        template = (
+            Bitbucket(client=cls.test_client)
+            .data
+            .get('_links', {})
+            .get('repositoryTagByName', {})
+            .get('href'))
+        cls.url = expand(
+            template, {
+                'bitbucket_url': cls.test_client.get_bitbucket_url(),
+                'owner': cls.owner,
+                'repository_name': cls.repository_name,
+                'ref_name': cls.name
+            })
+
+    @httpretty.activate
+    def test_response_is_a_tag(self):
+        httpretty.register_uri(
+            httpretty.GET,
+            self.url,
+            content_type='application/json',
+            body=self.resource_data(),
+            status=200)
+        response = Tag.find_tag_by_ref_name_in_repository(
+            ref_name=self.name,
+            repository_name=self.repository_name,
+            owner=self.owner,
+            client=self.test_client)
+        assert isinstance(response, Tag)
+
+
+class TestFindingTagsInRepository(TagFixture):
+    @classmethod
+    def setup_class(cls):
+        template = (
+            Bitbucket(client=cls.test_client)
+            .data
+            .get('_links', {})
+            .get('repositoryTags', {})
+            .get('href'))
+        cls.url = expand(
+            template, {
+                'bitbucket_url': cls.test_client.get_bitbucket_url(),
+                'owner': cls.owner,
+                'repository_name': cls.repository_name
+            })
+
+    @httpretty.activate
+    def test_response_is_a_tag_generator(self):
+        httpretty.register_uri(
+            httpretty.GET,
+            self.url,
+            content_type='application/json',
+            body=self.resource_list_data(),
+            status=200)
+        response = Tag.find_tags_in_repository(
+            repository_name=self.repository_name,
+            owner=self.owner,
+            client=self.test_client)
+        assert isinstance(next(response), Tag)
+
+
+class TestFindingTagsForSelf(TagFixture):
+    @classmethod
+    def setup_class(cls):
+        template = (
+            Bitbucket(client=cls.test_client)
+            .data
+            .get('_links', {})
+            .get('repositoryTags', {})
+            .get('href'))
+        cls.url = expand(
+            template, {
+                'bitbucket_url': cls.test_client.get_bitbucket_url(),
+                'owner': cls.test_client.get_username(),
+                'repository_name': cls.repository_name
+            })
+
+    @httpretty.activate
+    def test_response_from_only_repository_is_a_tag_generator(self):
+        httpretty.register_uri(
+            httpretty.GET,
+            self.url,
+            content_type='application/json',
+            body=self.resource_list_data(),
+            status=200)
+        response = Tag.find_tags_in_repository(
+            repository_name=self.repository_name,
+            client=self.test_client)
+        assert isinstance(next(response), Tag)
+
+
+class TestFindingBranchByNameInRepository(BranchFixture):
+    @classmethod
+    def setup_class(cls):
+        template = (
+            Bitbucket(client=cls.test_client)
+            .data
+            .get('_links', {})
+            .get('repositoryBranchByName', {})
+            .get('href'))
+        cls.url = expand(
+            template, {
+                'bitbucket_url': cls.test_client.get_bitbucket_url(),
+                'owner': cls.owner,
+                'repository_name': cls.repository_name,
+                'ref_name': cls.name
+            })
+
+    @httpretty.activate
+    def test_response_is_a_branch(self):
+        httpretty.register_uri(
+            httpretty.GET,
+            self.url,
+            content_type='application/json',
+            body=self.resource_data(),
+            status=200)
+        response = Branch.find_branch_by_ref_name_in_repository(
+            ref_name=self.name,
+            repository_name=self.repository_name,
+            owner=self.owner,
+            client=self.test_client)
+        assert isinstance(response, Branch)
+
+
+class TestFindingBranchInRepository(BranchFixture):
+    @classmethod
+    def setup_class(cls):
+        template = (
+            Bitbucket(client=cls.test_client)
+            .data
+            .get('_links', {})
+            .get('repositoryBranches', {})
+            .get('href'))
+        cls.url = expand(
+            template, {
+                'bitbucket_url': cls.test_client.get_bitbucket_url(),
+                'owner': cls.owner,
+                'repository_name': cls.repository_name
+            })
+
+    @httpretty.activate
+    def test_response_is_a_branch_generator(self):
+        httpretty.register_uri(
+            httpretty.GET,
+            self.url,
+            content_type='application/json',
+            body=self.resource_list_data(),
+            status=200)
+        response = Branch.find_branches_in_repository(
+            repository_name=self.repository_name,
+            owner=self.owner,
+            client=self.test_client)
+        assert isinstance(next(response), Branch)
+
+
+class TestFindingBranchesForSelf(BranchFixture):
+    @classmethod
+    def setup_class(cls):
+        template = (
+            Bitbucket(client=cls.test_client)
+            .data
+            .get('_links', {})
+            .get('repositoryBranches', {})
+            .get('href'))
+        cls.url = expand(
+            template, {
+                'bitbucket_url': cls.test_client.get_bitbucket_url(),
+                'owner': cls.test_client.get_username(),
+                'repository_name': cls.repository_name
+            })
+
+    @httpretty.activate
+    def test_response_from_only_repository_is_a_branch_generator(self):
+        httpretty.register_uri(
+            httpretty.GET,
+            self.url,
+            content_type='application/json',
+            body=self.resource_list_data(),
+            status=200)
+        response = Branch.find_branches_in_repository(
+            repository_name=self.repository_name,
+            client=self.test_client)
+        assert isinstance(next(response), Branch)
+
+
+class TestAccessingTagLinks(TagFixture):
+    @classmethod
+    def get_link_url(cls, name):
+        return (
+            cls.response
+            .data
+            .get('links', {})
+            .get(name, {})
+            .get('href'))
+
+    @classmethod
+    def setup_class(cls):
+        cls.response = cls.example_object()
+        cls.commits_url = cls.get_link_url('commits')
+        cls.commits_data = cls.resource_list_data('Commit')
+
+    @httpretty.activate
+    def test_commits_returns_a_commit_generator(self):
+        httpretty.register_uri(
+            httpretty.GET,
+            self.commits_url,
+            content_type='application/json',
+            body=self.commits_data,
+            status=200)
+        response = self.response.commits()
+        assert isinstance(next(response), Commit)
+
+
+class TestAccessingBranchLinks(BranchFixture):
+    @classmethod
+    def get_link_url(cls, name):
+        return (
+            cls.response
+            .data
+            .get('links', {})
+            .get(name, {})
+            .get('href'))
+
+    @classmethod
+    def setup_class(cls):
+        cls.response = cls.example_object()
+        cls.commits_url = cls.get_link_url('commits')
+        cls.commits_data = cls.resource_list_data('Commit')
+
+    @httpretty.activate
+    def test_commits_returns_a_commit_generator(self):
+        httpretty.register_uri(
+            httpretty.GET,
+            self.commits_url,
+            content_type='application/json',
+            body=self.commits_data,
+            status=200)
+        response = self.response.commits()
+        assert isinstance(next(response), Commit)

5f3b46fb5831421a085667b09e97ef888a032f8d
Ian Buchanan <ibuchanan@atlassian.com>
2016-03-04 11:55:11 -0500

    fixed docstrings

--- pybitbucket/team.py
+++ pybitbucket/team.py
@@ -19,23 +19,23 @@ class Team(BitbucketBase):
     def is_type(data):
         return (Team.has_v2_self_url(data))
 
-    """
-    A convenience method for finding teams by the user's role.
-    The method is a generator Team objects.
-    """
     @staticmethod
     def find_teams_for_role(role=TeamRole.ADMIN, client=Client()):
+        """
+        A convenience method for finding teams by the user's role.
+        The method is a generator Team objects.
+        """
         TeamRole.expect_valid_value(role)
         return Bitbucket(client=client).teamsForRole(role=role)
 
-    """
-    A convenience method for finding a specific team.
-    In contrast to the pure hypermedia driven method on the Bitbucket
-    class, this method returns a User object, instead of the
-    generator.
-    """
     @staticmethod
     def find_team_by_username(username, client=Client()):
+        """
+        A convenience method for finding a specific team.
+        In contrast to the pure hypermedia driven method on the Bitbucket
+        class, this method returns a User object, instead of the
+        generator.
+        """
         return next(Bitbucket(client=client).teamByUsername(
             username=username))
 

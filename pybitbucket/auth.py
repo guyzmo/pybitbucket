54ddb7521aa679d8f9946a04e81d2ac6fca2a9cb
Guyzmo <guyzmo+github@m0g.net>
2016-12-25 20:15:45 +0100

    🚀 Session injection support in auth module
    to enable hijacking of the requests session backend
    by testing tools such as betamax.
    
    Signed-off-by: Guyzmo <guyzmo+github@m0g.net>

--- pybitbucket/auth.py
+++ pybitbucket/auth.py
@@ -35,8 +35,10 @@ class Authenticator(object):
             headers.update({'From': email})
         return headers
 
-    def start_http_session(self):
-        session = Session()
+    def start_http_session(self, session=None):
+        session = session or Session()
+        if not isinstance(session, Session):
+            raise TypeError('session argument shall be of Session type')
         session.headers.update(self.headers())
         return session
 
@@ -48,12 +50,12 @@ class Authenticator(object):
         response.raise_for_status()
         return response.json()['username']
 
-    def __init__(self, server_base_uri=None):
+    def __init__(self, server_base_uri=None, session=None):
         self.server_base_uri = server_base_uri or 'https://api.bitbucket.org'
         self.who_am_i_url = expand(
             '{+server_base_uri}/2.0/user',
             {'server_base_uri': self.server_base_uri})
-        self.session = self.start_http_session()
+        self.session = self.start_http_session(session or Session())
 
 
 class Anonymous(Authenticator):
@@ -62,8 +64,10 @@ class Anonymous(Authenticator):
 
 class BasicAuthenticator(Authenticator):
 
-    def start_http_session(self):
-        session = Session()
+    def start_http_session(self, session=None):
+        session = session or Session()
+        if not isinstance(session, Session):
+            raise TypeError('session argument shall be of Session type')
         session.headers.update(self.headers(email=self.client_email))
         session.auth = HTTPBasicAuth(self.username, self.password)
         return session
@@ -76,11 +80,15 @@ class BasicAuthenticator(Authenticator):
             username,
             password,
             client_email,
-            server_base_uri=None):
+            server_base_uri=None,
+            session=None):
         self.username = username
         self.password = password
         self.client_email = client_email
-        super(self.__class__, self).__init__(server_base_uri=server_base_uri)
+        super(BasicAuthenticator, self).__init__(
+                server_base_uri=server_base_uri,
+                session=session
+        )
 
 
 class OAuth1Authenticator(Authenticator):
@@ -91,7 +99,8 @@ class OAuth1Authenticator(Authenticator):
             client_email=None,
             access_token=None,
             access_token_secret=None,
-            server_base_uri=None):
+            server_base_uri=None,
+            session=None):
 
         self.client_key = client_key
         self.client_secret = client_secret
@@ -99,19 +108,24 @@ class OAuth1Authenticator(Authenticator):
         self.access_token = access_token
         self.access_token_secret = access_token_secret
         self.username = None
-        super(self.__class__, self).__init__(server_base_uri=server_base_uri)
+        super(OAuth1Authenticator, self).__init__(
+                server_base_uri=server_base_uri,
+                session=session
+        )
 
     def get_username(self):
         if not self.username:
             self.username = self.who_am_i()
         return self.username
 
-    def start_http_session(self):
-        session = OAuth1Session(
+    def start_http_session(self, session=None):
+        session = session or OAuth1Session(
             self.client_key,
             client_secret=self.client_secret,
             resource_owner_key=self.access_token,
             resource_owner_secret=self.access_token_secret)
+        if not isinstance(session, OAuth1Session):
+            raise TypeError('session argument shall be of OAuth1Session type')
         session.headers.update(self.headers(email=self.client_email))
         return session
 
@@ -135,7 +149,8 @@ class OAuth2Authenticator(Authenticator):
             client_name=None,
             client_description=None,
             auth_uri=None,
-            token_uri=None):
+            token_uri=None,
+            session=None):
         self.client_id = client_id
         self.client_secret = client_secret
         self.client_email = client_email
@@ -154,10 +169,15 @@ class OAuth2Authenticator(Authenticator):
             token_uri or expand(
                 '{+server_base_uri}/site/oauth2/access_token',
                 {'server_base_uri': self.server_base_uri}))
-        super(self.__class__, self).__init__(server_base_uri=server_base_uri)
+        super(OAuth2Authenticator, self).__init__(
+                server_base_uri=server_base_uri,
+                session=session
+        )
 
-    def start_http_session(self):
-        session = OAuth2Session(self.client_id)
+    def start_http_session(self, session=None):
+        session = session or OAuth2Session(self.client_id)
+        if not isinstance(session, OAuth2Session):
+            raise TypeError('session argument shall be of OAuth2Session type instead of {}'.format(type(session)))
         session.headers.update(self.headers(email=self.client_email))
         if not session.authorized:
             self.redirect_response = self.grant.obtain_authorization(

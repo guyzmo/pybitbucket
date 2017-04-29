6a2fba58fee3c3d01135b327a493360d0e2e9309
Guyzmo <guyzmo+github+pub@m0g.net>
2017-04-30 00:33:55 +0200

    loosen dependencies versions
    Signed-off-by: Guyzmo <guyzmo+github+pub@m0g.net>

--- pybitbucket/metadata.py
+++ pybitbucket/metadata.py
@@ -8,10 +8,10 @@ Information describing the project.
 """
 
 # The package name, which is also the "UNIX name" for the project.
-package = 'pybitbucket'
-project = "PyBitbucket"
+package = 'pybitbucket_fork'
+project = "Fork of PyBitbucket"
 project_no_spaces = project.replace(' ', '')
-version = '0.12.1'
+version = '0.12.2'
 description = 'Fork of a Python wrapper for the Bitbucket API'
 authors = ['Ian Buchanan', 'Bernard Pratz']
 authors_string = ', '.join(authors)
--- setup.py
+++ setup.py
@@ -283,22 +283,22 @@ setup_dict = dict(
     ],
     packages=find_packages(exclude=(TESTS_DIRECTORY,)),
     install_requires=[
-        'six >= 1.10, < 2',
-        'future >= 0.16, < 1',
-        'requests >= 2.12, < 3',
-        'oauthlib >= 2.0, < 3',
-        'requests_oauthlib >= 0.7, < 1',
-        'uritemplate >= 0.6, < 1',
-        'simplejson >= 3.10, < 4',
-        'voluptuous >= 0.9, < 1'
+        'six',
+        'future',
+        'requests',
+        'oauthlib',
+        'requests_oauthlib',
+        'uritemplate',
+        'simplejson',
+        'voluptuous'
         # your module dependencies
     ] + python_version_specific_requires,
     # Allow tests to be run with `python setup.py test'.
     tests_require=[
-        'pytest >= 2.7, < 3',
-        'mock >= 1.0, < 2',
-        'flake8 >= 2.4, < 3',
-        'httpretty >= 0.8, < 1',
+        'pytest',
+        'mock',
+        'flake8',
+        'httpretty',
     ],
     cmdclass={'test': TestAllCommand},
     zip_safe=False,  # don't use eggs
